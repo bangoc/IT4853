e7a5f7488ae69211a250ddc1b8bbdda92895dea5
Nguyen Ba Ngoc <nguyen_bn@hotmail.com>
2018-09-17 17:30:05 +0700

    Configure files.h

--- Demo/algorithm/resources/files.h
+++
@@ -1,6 +0,0 @@
-#include <string>
-
-const std::string sorted_list1 = "/home/bangoc/git/IT4853/Demo/algorithm/resources/sorted_list1.txt";
-const std::string sorted_list2 = "/home/bangoc/git/IT4853/Demo/algorithm/resources/sorted_list2.txt";
-const std::string sorted_list3 = "/home/bangoc/git/IT4853/Demo/algorithm/resources/sorted_list3.txt";
-const std::string unsorted_list1 = "/home/bangoc/git/IT4853/Demo/algorithm/resources/unsorted_list1.txt";
