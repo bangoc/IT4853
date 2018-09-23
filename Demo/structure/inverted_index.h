d09fc679d81e83b166d5fd23c3316afb23fff45f
Nguyen Ba Ngoc <nguyen_bn@hotmail.com>
2018-09-23 20:20:20 +0700

    Fix a bug in inverted index interface

--- Demo/structure/inverted_index.h
+++ Demo/structure/inverted_index.h
@@ -18,14 +18,12 @@ struct SimpleIndexEntry {
 };
 
 using SimpleIndex = std::vector<SimpleIndexEntry>;
-using IdToPath = std::map<std::int32_t, std::string>;
-using IdToTerm = std::map<std::int32_t, std::string>;
 
 bool BuildSimpleIndex(const std::string& in_folder, const std::string& out_folder);
 bool SerializeSimpleIndex(const std::string& file_path, const SimpleIndex& simple_index);
 bool ParseSimpleIndex(const std::string& file_path, SimpleIndex& out);
 
 std::ostream& operator<<(std::ostream& stream, const SimpleIndex& simple_index);
-std::istream& operator>>(std::istream& stream, const SimpleIndex& simple_index);
+std::istream& operator>>(std::istream& stream, SimpleIndex& simple_index);
 
 #endif  // STRUCTURE_INVERTED_INDEX_H_
